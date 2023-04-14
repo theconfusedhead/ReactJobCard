9b4ef0b2dfc7c227ff141e461ead9977a7a9cbcb
Shivam Chourasia <shivamchourasia2212@gmail.com>
2023-04-14 20:11:53 +0530

    dark mode

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
+import React from 'react';
 import './App.css';
 import Name from "./Name";
 import Body from "./Body";
 import Footer from './footer';
 import CoverImage from './CoverImage'
 function App() {
+  const [mode, setMode] = React.useState(false);
+  function HandleMode(){
+    setMode(prevMode => !prevMode)
+  }
   return (
+    <>
+    <div className='nav'>
+    <button  onClick={HandleMode}>
+      Click for {mode? 'Light Mode' : 'Dark Mode'}
+    </button>
+    </div>
   <div className='main-box'>
-    <div className='content-box'>
+    <div className={mode ? 'content-box--light' : 'content-box--dark'}>
       <CoverImage/>
       <Name/>
       <Body/>
       <Footer/>
    </div>
   </div>
-    
+  </>
   );
 }
 
